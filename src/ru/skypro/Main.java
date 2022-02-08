27ae61faa4fd533e8a14f1a4e0df9bdb75c4e643
Ruslan <gimazevs@mail.ru>
2022-02-08 21:09:04 +0300

    первое задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -8,7 +8,15 @@ public class Main {
         System.out.println(test[1]);
         System.out.println(test[2]);
 
+        float[] test2 = {1.57f, 7.654f, 9.986f};
 
-
+        double[] random = new double[4];
+        random[0] = 90000;
+        random[1] = 2.77;
+        random[2] = 3.996589;
+        random[3] = 7.785963214598;
+        for (int i = 0; i < 4; i++) {
+            System.out.println(random[i]);
+        }
     }
 }
