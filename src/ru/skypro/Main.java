d7d53019d87e87daa56e98f45fc1139411f0cc4f
Ruslan <gimazevs@mail.ru>
2022-02-08 20:57:41 +0300

    первый коммит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,12 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] test = new int[3];
+        System.out.println(test[0]);
+        System.out.println(test[1]);
+        System.out.println(test[2]);
+
+
+
     }
 }
