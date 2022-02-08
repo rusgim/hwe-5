a93baf62e0656e041593eb0f079ac6e1710b10cd
Ruslan <gimazevs@mail.ru>
2022-02-08 21:49:54 +0300

    третье задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         int[] test = new int[3];
-        System.out.println(test[0]);
-        System.out.println(test[1]);
-        System.out.println(test[2]);
+        test[0] = 1;
+        test[1] = 2;
+        test[2] = 3;
 
         float[] test2 = {1.57f, 7.654f, 9.986f};
 
@@ -21,5 +21,10 @@ public class Main {
         System.out.print(test[0] + ", " + test[1] + ", " + test[2]);
         System.out.print("\n" + test2[0] + ", " + test2[1] + ", " + test2[2]);
         System.out.print("\n" + random[0] + ", " + random[1] + ", " + random[2] + ", " + random[3]);
+        System.out.println("\n");
+
+        System.out.print(test[2] + ", " + test[1] + ", " + test[0]);
+        System.out.println("\n" + test2[2] + ", " + test2[1] + ", " + test2[0]);
+        System.out.println(random[3] + ", " + random[2] + ", " + random[1] + ", " + random[0]);
     }
 }
