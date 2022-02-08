311825d1de968824078453e7168bd90b3270f691
Ruslan <gimazevs@mail.ru>
2022-02-08 23:08:19 +0300

    4 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,9 +7,16 @@ public class Main {
         test[0] = 1;
         test[1] = 2;
         test[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            test[i] = test[i] + 1;
+            if (i % 2 == 0) {
+                System.out.print(test[i] + " ");
+            }
+        }
+
 
         float[] test2 = {1.57f, 7.654f, 9.986f};
-
+        System.out.println("\n");
         double[] random = new double[4];
         random[0] = 90000;
         random[1] = 2.77;
@@ -26,5 +33,6 @@ public class Main {
         System.out.print(test[2] + ", " + test[1] + ", " + test[0]);
         System.out.println("\n" + test2[2] + ", " + test2[1] + ", " + test2[0]);
         System.out.println(random[3] + ", " + random[2] + ", " + random[1] + ", " + random[0]);
+
     }
 }
