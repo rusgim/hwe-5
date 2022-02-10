741063ced6dca5ad681b1539d1d01757c911b66e
Ruslan <gimazevs@mail.ru>
2022-02-10 21:30:02 +0300

    не выводит в консоль, помогите пожалуйста

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,12 +7,7 @@ public class Main {
         test[0] = 1;
         test[1] = 2;
         test[2] = 3;
-        //for (int i = 0; i < 3; i++) {
-        // test[i] = test[i] + 1;
-        //  if (i % 2 == 0) {
-        //      System.out.print(test[i] + " ");
-        //    }
-        //  }
+
         float[] test2 = {1.57f, 7.654f, 9.986f};
         System.out.println("\n");
         double[] random = new double[4];
@@ -27,7 +22,6 @@ public class Main {
             } else {
                 System.out.print(test[i] + ",");
             }
-
         }
         for (int i = 0; i < test2.length; i++) {
             if (i == test2.length - 1) {
@@ -35,7 +29,6 @@ public class Main {
             } else {
                 System.out.print(test2[i] + ",");
             }
-
         }
         for (int i = 0; i < random.length; i++) {
             if (i == random.length - 1) {
@@ -44,18 +37,26 @@ public class Main {
                 System.out.print(random[i] + ", ");
             }
         }
-        System.out.print("\n");
-        int testSize = test.length;
-        float test2Size = test2.length;
-        double randomSize = random.length;
-        int i = testSize;
-        for (; i >= test[0]; i--) {
-            if (i == test[0]) {
-                System.out.println(test[i]);
+       for (int i = random.length; i == 0; i--) {
+            if (i == 0) {
+                System.out.println(random[random.length]);
             } else {
-                System.out.print(test[i] + ",");
+                System.out.print(random[random.length] + ", ");
             }
         }
-
+       for (int i = test.length; i == 0; i--) {
+            if (i == 0) {
+                System.out.println(test[i]);
+            } else {
+                System.out.print(test[i] + ", ");
+            }
+        }
+        for (int i = test2.length; i == 0; i--) {
+            if (i == 0) {
+                System.out.println(test2[i]);
+            } else {
+                System.out.print(test2[i] + ", ");
+            }
+        }
     }
 }
