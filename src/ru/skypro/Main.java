a93604c87fe7ac65cb1016c7e058d310e086fa53
Ruslan <gimazevs@mail.ru>
2022-02-10 06:02:43 +0300

    исправил второе задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,12 +7,12 @@ public class Main {
         test[0] = 1;
         test[1] = 2;
         test[2] = 3;
-        for (int i = 0; i < 3; i++) {
-            test[i] = test[i] + 1;
-            if (i % 2 == 0) {
-                System.out.print(test[i] + " ");
-            }
-        }
+        //for (int i = 0; i < 3; i++) {
+        // test[i] = test[i] + 1;
+        //  if (i % 2 == 0) {
+        //      System.out.print(test[i] + " ");
+        //    }
+        //  }
         float[] test2 = {1.57f, 7.654f, 9.986f};
         System.out.println("\n");
         double[] random = new double[4];
@@ -20,16 +20,29 @@ public class Main {
         random[1] = 2.77;
         random[2] = 3.996589;
         random[3] = 7.785963214598;
-        for (int i = 0; i < 4; i++) {
-            System.out.println(random[i]);
-        }
-        System.out.print(test[0] + ", " + test[1] + ", " + test[2]);
-        System.out.print("\n" + test2[0] + ", " + test2[1] + ", " + test2[2]);
-        System.out.print("\n" + random[0] + ", " + random[1] + ", " + random[2] + ", " + random[3]);
-        System.out.println("\n");
 
-        System.out.print(test[2] + ", " + test[1] + ", " + test[0]);
-        System.out.println("\n" + test2[2] + ", " + test2[1] + ", " + test2[0]);
-        System.out.println(random[3] + ", " + random[2] + ", " + random[1] + ", " + random[0]);
+        for (int i = 0; i < test.length; i++) {
+            if (i == test.length - 1) {
+                System.out.println(test[i]);
+            } else {
+                System.out.print(test[i] + ",");
+            }
+
+            }
+        for (int i = 0; i < test2.length; i++) {
+            if (i == test2.length - 1) {
+                System.out.println(test2[i]);
+            } else {
+                System.out.print(test2[i] + ",");
+            }
+            
+        }
+        for (int i = 0; i < random.length; i++) {
+            if (i == random.length - 1) {
+                System.out.println(random[i]);
+            } else {
+                System.out.print(random[i] + ", ");
+            }
+        }
+        }
     }
-}
